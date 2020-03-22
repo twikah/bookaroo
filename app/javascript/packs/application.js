3ad848d8466a6b5b152d4a1351aeb6024622fc64
Vanessa Santo <vanessaesanto@gmail.com>
2020-03-22 11:50:39 +0000

    Home page done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,27 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import AOS from 'aos';
+
+AOS.init();
+
+// Change background color on home page
+// const serviceSections = document.querySelectorAll('.service-section');
+// const colors = ['#FAF9F6', '#CF5C36']
+
+// console.log('here', serviceSections)
+// window.addEventListener('scroll', (event) => {
+//   const scrollFromTop = window.pageYOffset;
+
+//   for (let i = 0; i < serviceSections.length; i++) {
+//     const sectionBottom = serviceSections[i].offsetTop + serviceSections[i].offsetHeight * 0.6
+
+//     console.log('and here', i, scrollFromTop, 'lol', sectionBottom)
+
+//     if (scrollFromTop <= sectionBottom) {
+//       document.querySelector('body').style.backgroundColor =
+//         colors[i % colors.length];
+//       break;
+//     }
+//   }
+// })
